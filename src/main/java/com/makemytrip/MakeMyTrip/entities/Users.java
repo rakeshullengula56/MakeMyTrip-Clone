5b4889836df521077b01b4b872e464115bd3ed36
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-03-02 21:21:44 +0530

    created User entity to handle customer and Admin roles

---
+++ src/main/java/com/makemytrip/MakeMyTrip/entities/Users.java
@@ -0,0 +1,34 @@
+package com.makemytrip.MakeMyTrip.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document(collection = "users")
+public class Users {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+    private String phoneNumber;
+    private List<Booking> bookings;
+
+    @Setter
+    @Getter
+    public static class Booking {
+        private String type;
+        private String bookingId;
+        private String date;
+        private int quantity;
+        private double totalPrice;
+
+    }
+}
